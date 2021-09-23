a5ae5645e95c87546f8ab2327ef8424ba8d24cac
Dhruv Pande <46739476+dhruvpande-qait@users.noreply.github.com>
2021-09-23 10:24:49 +0530

    Update Test1.java

--- testFlakey/src/test/java/com/dhruv/test/testFlakey/testPackage/Test1.java
+++ testFlakey/src/test/java/com/dhruv/test/testFlakey/testPackage/Test1.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class Test1 {
 	@Test
 	public void test() {
-		Assert.assertEquals("1", "3","Assertion Falied: First test failed");
+		Assert.assertEquals("1", "34","Assertion Falied: First test failed");
 		System.out.println("First Test Passed");
 	}
 }
